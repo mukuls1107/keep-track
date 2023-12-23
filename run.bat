96f6579920f16a9de3fa609029c5b368e7714a5b
mukul1107 <mukulsingh3078@gmail.com>
2023-12-23 22:14:02 +0530

    Fixed Remove Task ✅

--- run.bat
+++ run.bat
@@ -1,1 +1,22 @@
-task.exe
+@echo off
+
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+if %errorlevel% neq 0 (
+    echo You must run this script as Administrator.
+    pause
+    goto :eof
+)
+
+set filename=main
+
+gcc %filename%.c -o %filename%.exe 2> error_log.txt
+
+if %errorlevel% equ 0 (
+    echo Compilation successful!
+    %filename%.exe
+) else (
+    echo Compilation failed.
+)
+
+pause
